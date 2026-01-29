05e90d670571dbabb3b541b068623480d29725f1
Maxence LAURENCE <maxence.laurence.pro@gmail.com>
2026-01-29 05:10:39 +0100

    modification de seed.ts : ajoue de 2 deck par défaut

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,15 @@
 import bcrypt from "bcryptjs";
-import {readFileSync} from "fs";
-import {join} from "path";
-import {prisma} from "../src/database";
-import {CardModel} from "../src/generated/prisma/models/Card";
-import {PokemonType} from "../src/generated/prisma/enums";
+import { readFileSync } from "fs";
+import { join } from "path";
+import { prisma } from "../src/database";
+import { CardModel } from "../src/generated/prisma/models/Card";
+import { PokemonType } from "../src/generated/prisma/enums";
 
 async function main() {
     console.log("🌱 Starting database seed...");
 
+    await prisma.deckCard.deleteMany();
+    await prisma.deck.deleteMany();
     await prisma.card.deleteMany();
     await prisma.user.deleteMany();
 
@@ -28,8 +30,8 @@ async function main() {
         ],
     });
 
-    const redUser = await prisma.user.findUnique({where: {email: "red@example.com"}});
-    const blueUser = await prisma.user.findUnique({where: {email: "blue@example.com"}});
+    const redUser = await prisma.user.findUnique({ where: { email: "red@example.com" } });
+    const blueUser = await prisma.user.findUnique({ where: { email: "blue@example.com" } });
 
     if (!redUser || !blueUser) {
         throw new Error("Failed to create users");
@@ -57,6 +59,50 @@ async function main() {
     );
 
     console.log(`✅ Created ${pokemonData.length} Pokemon cards`);
+
+    await prisma.deck.createMany({ // création des decks 
+        data: [
+            {
+                name: "Starter Deck",
+                userId: redUser.id,
+            },
+            {
+                name: "Starter Deck",
+                userId: blueUser.id,
+            },
+        ],
+    });
+
+    console.log("✅ Created decks:", redUser.username, blueUser.username);
+
+    const redDeck = await prisma.deck.findFirst({ where: { userId: redUser.id } }); // Récupérer les id car autoincrémentation (donc pas forcément 1 et 2 !!) 
+    const blueDeck = await prisma.deck.findFirst({ where: { userId: blueUser.id } });
+
+    if (!redDeck || !blueDeck) {
+        throw new Error("Decks not found");
+    }
+
+    const redDeckCards = Array.from({ length: 10 }).map(() => { // 10 carte aléatoire pour le deck de l'user rouge
+        const randomCard = createdCards[Math.floor(Math.random() * createdCards.length)];
+        return {
+            deckId: redDeck.id, // l'id du deck de l'user rouge
+            cardId: randomCard.id, // carte aléatoire
+        };
+    });
+
+    const blueDeckCards = Array.from({ length: 10 }).map(() => { // 10 carte aléatoire pour le deck de l'user bleu 
+        const randomCard = createdCards[Math.floor(Math.random() * createdCards.length)];
+        return {
+            deckId: blueDeck.id, // l'id du deck de l'user bleu
+            cardId: randomCard.id, // carte aléatoire
+        };
+    });
+
+    await prisma.deckCard.createMany({
+        data: [...redDeckCards, ...blueDeckCards], // les "..." = les 10 élément de redDeckCards et blueDeckCards
+    });
+
+    console.log("✅ Created deckcards");
 
     console.log("\n🎉 Database seeding completed!");
 }
