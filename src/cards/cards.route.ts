90ef0cdbc1345bf71f8ad98242d56184ff2cc58f
Maxence LAURENCE <maxence.laurence.pro@gmail.com>
2026-02-06 09:27:02 +0100

    ajoue de l'endpoint GET /api/cards

---
+++ src/cards/cards.route.ts
@@ -0,0 +1,23 @@
+import { Request, Response, Router } from 'express'
+import { prisma } from "../database";
+import { authenticateToken } from '../auth/auth.middleware'
+
+export const cardsRouter = Router()
+
+// GET /api/cards 
+// Retourne la liste des cards 
+cardsRouter.get('', authenticateToken, async (_req: Request, res: Response) => { // faire authenticateToken puis ensuite notre fonction : 
+    try {
+        const cardsList = await prisma.card.findMany({
+            orderBy: {
+                pokedexNumber: 'asc'
+            }
+        })
+
+        return res.status(200).json({ cardsList })
+    }
+    catch (error) {
+        console.error('Erreur lors de la connexion:', error)
+        return res.status(500).json({ error: 'Erreur serveur' })
+    }
+})
