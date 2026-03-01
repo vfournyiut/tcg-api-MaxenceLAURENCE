b7ac178b3b94640531dfbaaad4755e4ab9c62a52
MaxenceLAURENCE <maxence.laurence.pro@gmail.com>
2026-03-01 15:43:54 +0100

    ci(prettier): correction de la configuration de prettier et lint

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,15 +1,15 @@
 import type { Config } from 'prettier'
 
 const config: Config = {
-  singleQuote: true,
-  trailingComma: 'all',
-  semi: false,
-  printWidth: 80,
-  tabWidth: 2,
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  endOfLine: 'lf',
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    printWidth: 80,
+    tabWidth: 4,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
 }
 
 export default config
